423afaf9cb08d1d7ea7df67787af8e1baba833ba
salsabilla16012006@gmail.com <salsabilla16012006@gmail.com>
2024-01-22 11:14:15 +0700

    menambah firebase aa kasian aa

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js";
+import {
+getFirestore,
+collection, 
+addDoc, 
+getDocs, 
+deleteDoc, 
+doc, 
+query, 
+orderby
+} from "https://www.gstatic.com/firebasejs/10.7.2/firebase-firestore.js";
 
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCbewR7btYDgZ8EWsuSJJzTTVHkV4p--F0",
+  authDomain: "cipi-capa.firebaseapp.com",
+  projectId: "cipi-capa",
+  storageBucket: "cipi-capa.appspot.com",
+  messagingSenderId: "657661237676",
+  appId: "1:657661237676:web:d55836db470ebd92a34209",
+  measurementId: "G-NW59BY1TN1"
+};
+
+const app = initializeApp(firebaseConfig);
